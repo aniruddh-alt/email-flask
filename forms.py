7fe8f6da5d6c286b4a5f11096e7032e6b2d72d4a
aniruddh-alt <77236089+aniruddh-alt@users.noreply.github.com>
2023-01-25 19:26:47 -0500

    first commit

--- forms.py
+++ forms.py
@@ -6,7 +6,7 @@ from wtforms.validators import DataRequired, Length
 
 class email_form(FlaskForm):
     sender_email = EmailField("Sender's Email", validators=[DataRequired(), Length(3, 50)])
-    receiver_email = EmailField("Reciever's Email", validators=[DataRequired(), Length(3, 50)])
+    receiver_email = EmailField("Receiver's Email", validators=[DataRequired(), Length(3, 50)])
     subject = StringField("Subject")
     message = StringField("Message", validators=[DataRequired()])
     submit = SubmitField("submit")
