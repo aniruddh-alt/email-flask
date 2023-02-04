440366c787371a0bd1ed1f5c7b767335258b5c84
aniruddh-alt <77236089+aniruddh-alt@users.noreply.github.com>
2023-02-04 17:32:16 -0500

    file commit

--- forms.py
+++ forms.py
@@ -1,12 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import EmailField, StringField, SubmitField
+from wtforms import EmailField, StringField, SubmitField,FileField
 from wtforms import validators
 from wtforms.validators import DataRequired, Length
 
 
 class email_form(FlaskForm):
-    sender_email = EmailField("Sender's Email", validators=[DataRequired(), Length(3, 50)])
-    receiver_email = EmailField("Receiver's Email", validators=[DataRequired(), Length(3, 50)])
+    sender_email = StringField("Sender's Email", validators=[DataRequired(), Length(3, 50)])
+    receiver_email = StringField("Receiver's Email", validators=[DataRequired(), Length(3, 50)])
     subject = StringField("Subject")
-    message = StringField("Message", validators=[DataRequired()])
+    message = StringField("Message")
+    html = StringField("HTML Body Text")
+    file = FileField("File")
     submit = SubmitField("submit")
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+import json
+import os
 from flask import Flask, render_template, flash
 from forms import *
 import requests
+from fileinput import filename
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "12345678"
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=["GET", 'POST'])
 def home():
     form = email_form()
     if form.validate_on_submit():
@@ -15,18 +18,24 @@ def home():
         to_emails = form.receiver_email.data
         subject = form.subject.data
         body = form.message.data
-        requestt(to_emails=to_emails, from_email=from_emails, subject=subject, body=body)
+        html = form.html.data
+        file = form.file.data
+        file.save(file.filename)
+
+        requestt(to_emails=to_emails, from_email=from_emails, subject=subject, body=body, file=file, html_body=html)
 
     return render_template("Home.html", form=form)
 
 
 @app.route("/email", methods=['GET', 'POST'])
-def requestt(to_emails, subject, body, from_email="aniruddhusa04@gmail.com", test_mode=False, api_key=123):
+def requestt(to_emails, subject, body, file, html_body, from_email="aniruddhusa04@gmail.com", test_mode=False,
+             api_key='pYkqe7Y96A2PgbTlcgZ5', ):
     url = 'https://chi-tools.uc.edu/email_service/api/send_mail'
     data = {'api_key': api_key, 'to_emails': to_emails, 'subject': subject, 'body': body,
-            'from_email': from_email, 'test_mode': test_mode}
-    r = requests.post(url=url, json=data)
-    return r
+            'from_email': from_email, 'test_mode': test_mode, 'html_body': html_body}
+    files = {'files': open(file.filename, "rb")}
+    requests.post(url=url, data=data, files=files)
+    return "Email Sent"
 
 
 if __name__ == "__main__":
